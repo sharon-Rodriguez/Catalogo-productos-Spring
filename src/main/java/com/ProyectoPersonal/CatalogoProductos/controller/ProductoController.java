030644aab8cb4649babc1f597376f8000e8e2cab
Sharon Rodriguez <sharo2599@gmail.com>
2026-05-18 17:42:09 -0500

    feat: crear controller

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/ProductoController.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/ProductoController.java
@@ -1,4 +1,31 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
+import com.ProyectoPersonal.CatalogoProductos.model.Producto;
+import com.ProyectoPersonal.CatalogoProductos.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
 public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    @GetMapping
+    public Producto verProductoID (@PathVariable Long id){
+        return productoService.verProductosId(id);
+    }
+
+    @GetMapping
+    public List<Producto> verProductos (){
+        return productoService.verProductos();
+    }
+
+    @PostMapping
+    public Producto crearProducto (@RequestBody Producto producto){
+        return productoService.crearProducto(producto);
+    }
 }
