ebcd0cb3922dba3cc7e8fe7d296c3472b4093f02
Sharon Rodriguez <sharo2599@gmail.com>
2026-05-18 13:08:02 -0500

    feat: iniciar Catalogo-Productos con Sping boot

---
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/CatalogoProductosApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoPersonal.CatalogoProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProductosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProyectoPersonal/CatalogoProductos/CatalogoProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPersonal.CatalogoProductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
