e28239c549c0c4b8b6137532b2f773f7f968e3c1
Sharon Rodriguez <sharo2599@gmail.com>
2026-06-08 12:53:33 -0500

    feat: agregar repository de Usuario

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/repository/UsuarioRepository.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/repository/UsuarioRepository.java
@@ -1,4 +1,12 @@
 package com.ProyectoPersonal.CatalogoProductos.repository;
 
-public interface UsuarioRepository {
+import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
