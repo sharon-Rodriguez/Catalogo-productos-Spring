6d41b8f6278b061b94cc120d15730a1c907431b4
Sharon Rodriguez <sharo2599@gmail.com>
2026-06-08 13:02:57 -0500

    feat: agragar servicio usuario

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/service/UsuarioService.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/service/UsuarioService.java
@@ -1,4 +1,22 @@
 package com.ProyectoPersonal.CatalogoProductos.service;
 
+import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
+import com.ProyectoPersonal.CatalogoProductos.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public Usuario registro (Usuario usuario){
+        usuario.setContraseña(passwordEncoder.encode(usuario.getContraseña()));
+        return usuarioRepository.save(usuario);
+    }
 }
