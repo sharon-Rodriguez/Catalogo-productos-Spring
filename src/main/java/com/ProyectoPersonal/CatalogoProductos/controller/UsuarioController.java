ad086aca5e607e98b8fa0f3943f7377bf17100a6
Sharon Rodriguez <sharo2599@gmail.com>
2026-06-08 12:31:22 -0500

    feat: agregar controlador de usuario

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/UsuarioController.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/UsuarioController.java
@@ -1,4 +1,23 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
+import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
+import com.ProyectoPersonal.CatalogoProductos.security.ServicesUsuario;
+import com.ProyectoPersonal.CatalogoProductos.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping ("/auth")
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService servicesUsuario;
+
+    @PostMapping ("/registro")
+    public Usuario registro (@RequestBody Usuario usuario){
+        return servicesUsuario.registro(usuario);
+    }
 }
