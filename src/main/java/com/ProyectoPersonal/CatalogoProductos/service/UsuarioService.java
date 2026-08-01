270f29603e775f85c7386e9ebfee8b90fdd89591
Sharon Rodriguez <sharo2599@gmail.com>
2026-07-31 22:19:06 -0500

    feat: Crear un metodo para mostrar usuario por id y otro metodo paramostrar todos los usuarios

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/service/UsuarioService.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/service/UsuarioService.java
@@ -1,10 +1,13 @@
 package com.ProyectoPersonal.CatalogoProductos.service;
 
+import com.ProyectoPersonal.CatalogoProductos.model.MuestraUsuario;
 import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
 import com.ProyectoPersonal.CatalogoProductos.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -14,6 +17,18 @@ public class UsuarioService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    public List<MuestraUsuario> mostrarUsuariosTodos (){
+        return usuarioRepository.findAll()
+                .stream()
+                .map(e -> new MuestraUsuario(e.getNombre(), e.getEmail()))
+                .toList();
+    }
+
+    public Usuario mostrarUsuario (Long id){
+        return usuarioRepository.findById(id)
+                .orElseThrow(()->new RuntimeException("Usuario con id =" + id + "no encontrado"));
+    }
 
     public Usuario registro (Usuario usuario){
         usuario.setContraseña(passwordEncoder.encode(usuario.getContraseña()));
