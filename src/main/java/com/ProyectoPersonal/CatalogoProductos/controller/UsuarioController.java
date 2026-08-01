c0582effb6807ab080f8e0bf59e0ea30f6ee8ed3
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-01 14:03:41 -0500

    feat: Agregar en controller el metodo que muestra todos los usuarios

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/UsuarioController.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
+import com.ProyectoPersonal.CatalogoProductos.model.MuestraUsuario;
 import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
 import com.ProyectoPersonal.CatalogoProductos.security.ServicesUsuario;
 import com.ProyectoPersonal.CatalogoProductos.service.UsuarioService;
@@ -7,12 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping ("/auth")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService servicesUsuario;
+
+    @GetMapping ("/auth")
+    public List<MuestraUsuario> usuarios (){
+        return servicesUsuario.mostrarUsuariosTodos();
+    }
 
     @GetMapping ("/{id}")
     public Usuario mostrarUsuario (@PathVariable Long id){
