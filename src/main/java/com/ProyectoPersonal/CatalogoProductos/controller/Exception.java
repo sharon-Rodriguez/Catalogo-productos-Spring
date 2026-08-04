0d61da3c0446ebc7fffd63800d1592b331d6657c
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-03 20:05:04 -0500

    feat: Crear clase para manejar el error 404 para mostrar mensaje en postman

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/Exception.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/Exception.java
@@ -1,4 +1,16 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class Exception {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String manejarError (RuntimeException e){
+        return e.getMessage();
+    }
 }
