97ce7d6bad62463cadc06630abfc8d17be8e7885
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-03 20:36:40 -0500

    feat: construir creacion de token y validacion de mismo

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/security/TokenServices.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/security/TokenServices.java
@@ -1,4 +1,33 @@
 package com.ProyectoPersonal.CatalogoProductos.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
 public class TokenServices {
+    private String secret = "sharonteamo";
+
+    public String generarToken (String email){
+        return JWT.create()
+                .withSubject(email)
+                .withExpiresAt(Instant.now().plus(2, ChronoUnit.HOURS))
+                .sign(Algorithm.HMAC256(secret));
+    }
+
+    public String validarToken (String token){
+        try {
+            return JWT.require(Algorithm.HMAC256(secret))
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException e){
+            return null;
+        }
+    }
+
 }
