da5c5c81e9d10410664304cb073c90f444b6c7c7
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-03 20:02:27 -0500

    feat: Crear clase para manejar el login aparte

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/AuthController.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/AuthController.java
@@ -1,4 +1,36 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
+import com.ProyectoPersonal.CatalogoProductos.model.Usuario;
+import com.ProyectoPersonal.CatalogoProductos.security.TokenServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/auth")
 public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private TokenServices tokenServices;
+
+    @PostMapping("/auth")
+    public String login (@RequestBody Usuario usuario){
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        usuario.getEmail(),
+                        usuario.getContraseña()
+                )
+        );
+        Usuario usuarioAutorizado = (Usuario) authentication.getPrincipal();
+        return tokenServices.generarToken(usuarioAutorizado.getEmail());
+    }
+
 }
