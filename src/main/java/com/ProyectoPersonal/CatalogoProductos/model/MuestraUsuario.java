d9f3966f4205a3c6c43d9dad222d88619bd506da
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-03 20:09:41 -0500

    chore: Crear record para mostrar ciertos campos de usuairo

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/model/MuestraUsuario.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/model/MuestraUsuario.java
@@ -1,4 +1,4 @@
 package com.ProyectoPersonal.CatalogoProductos.model;
 
-public record MuestraUsuario(String nombre, String email) {
+public record MuestraUsuario(String nombre, String email, Long id,Enum rol) {
 }
