28e83f299d47788739592c2d2b3fbb01f002c0de
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-04 22:22:03 -0500

    chore: agregar mensajes de error para usuario erroneo y contraseña

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/Exception.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/Exception.java
@@ -1,6 +1,8 @@
 package com.ProyectoPersonal.CatalogoProductos.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,5 +14,17 @@ public class Exception {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public String manejarError (RuntimeException e){
         return e.getMessage();
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String manejarUsuarioNoEncontrado(UsernameNotFoundException e) {
+        return "El email no existe";
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public String manejarContraseñaIncorrecta(BadCredentialsException e) {
+        return "La contraseña es incorrecta";
     }
 }
