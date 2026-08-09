3ecb73a822aa98ffcb5f46a73069d9efa887d831
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-08 20:57:56 -0500

    feat: crear clase para recibir los datos de la api

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/model/ApiDatos_traer.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/model/ApiDatos_traer.java
@@ -1,4 +1,12 @@
 package com.ProyectoPersonal.CatalogoProductos.model;
 
+import java.util.Map;
+
 public class ApiDatos_traer {
+    private String result;
+    private Map<String, Double> conversion_rates;
+
+//    getters
+    public String getResult() { return result; }
+    public Map<String, Double> getConversion_rates() { return conversion_rates; }
 }
