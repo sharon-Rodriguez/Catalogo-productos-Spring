075cee98f7f824f73186243ed0f3832c03c37350
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-08 20:03:50 -0500

    fix: arreglar endpoint mal escrito

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/AuthController.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/controller/AuthController.java
@@ -6,10 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/auth")
@@ -21,7 +18,7 @@ public class AuthController {
     @Autowired
     private TokenServices tokenServices;
 
-    @PostMapping("/auth/login")
+    @PostMapping("/login")
     public String login (@RequestBody Usuario usuario){
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
