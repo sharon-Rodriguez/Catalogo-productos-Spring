0bfb5c7aae7738cf7c3fae9176b13967879275c9
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-08 19:33:10 -0500

    feat: agragar if para que no se haga validacion de token

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/security/JwtFilter.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/security/JwtFilter.java
@@ -28,6 +28,14 @@ public class JwtFilter extends OncePerRequestFilter {
                                     FilterChain filterChain)
             throws ServletException, IOException {
 
+        String requestPath = request.getRequestURI();
+
+        // Permite login y registro sin token
+        if (requestPath.contains("/auth/login") || requestPath.contains("/auth/registro")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
