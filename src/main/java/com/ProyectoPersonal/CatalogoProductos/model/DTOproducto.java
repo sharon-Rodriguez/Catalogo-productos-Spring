09462c82f974571c22f1b12faf63b789c338847c
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-12 22:10:42 -0500

    feat: crear DTO para poder usar el model producto con el precio del producto formateado

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/model/DTOproducto.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/model/DTOproducto.java
@@ -1,4 +1,33 @@
 package com.ProyectoPersonal.CatalogoProductos.model;
 
 public class DTOproducto {
+
+    private Long id;
+    private String nombre;
+    private String precio ;
+    private Integer cantidad;
+
+    public DTOproducto(Producto p) {
+        this.id = p.getId();
+        this.nombre = p.getNombre();
+        this.precio = p.getPrecioFormateado();
+        this.cantidad = p.getCantidad();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
 }
