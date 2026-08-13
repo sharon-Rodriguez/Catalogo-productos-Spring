b36024a9f25067eb5d20a1941453dd386fad8727
Sharon Rodriguez <sharo2599@gmail.com>
2026-08-12 22:07:52 -0500

    feat: añadir metodo para usar el precio en texto al mostrarlo al usuairo

--- src/main/java/com/ProyectoPersonal/CatalogoProductos/model/Producto.java
+++ src/main/java/com/ProyectoPersonal/CatalogoProductos/model/Producto.java
@@ -2,17 +2,23 @@ package com.ProyectoPersonal.CatalogoProductos.model;
 
 import jakarta.persistence.*;
 
+import java.text.DecimalFormat;
+
 @Entity
 @Table(name = "productos")
 
 public class Producto {
 
+    private static final DecimalFormat DF = new DecimalFormat();
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    public Long getId() { return id; }
+
     private String nombre;
-    private Double precio;
+    private Double precio ;
     private Integer  cantidad;
 
     public Producto() {
@@ -24,7 +30,10 @@ public class Producto {
     public void setCantidad(Integer cantidad) { this.cantidad = cantidad; }
 
    //    getter
-    public Long getId() { return id; }
+    public String getPrecioFormateado (){
+        if (precio == null) return "0";
+        return DF.format(precio);
+    }
     public String getNombre() { return nombre; }
     public Double getPrecio() { return precio; }
     public Integer getCantidad() { return cantidad; }
